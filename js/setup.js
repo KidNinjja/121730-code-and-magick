e7b97e731464680e430f2e8b8d9a1c78b3076730
RudnevDm <www.rudnev.web@gmail.com>
2017-02-19 14:32:55 +0300

    Внес правки в соответствии с рекомендациями наставника

--- js/setup.js
+++ js/setup.js
@@ -2,24 +2,7 @@
 
   /* Получение случайного числа */
   var getRandomInteger = function (min, max, flag) {
-    if (flag) {
-
-      var bondingColor = [];
-      var currentBondingColor = '';
-
-      bondingColor.push((Math.floor(Math.random() * (max - min)) + min).toString(16));
-      bondingColor.push((Math.floor(Math.random() * (max - min)) + min).toString(16));
-      bondingColor.push((Math.floor(Math.random() * (max - min)) + min).toString(16));
-
-      for (var i = 0; i < bondingColor.length; i++) {
-        currentBondingColor += bondingColor[i];
-      }
-
-      return currentBondingColor;
-
-    } else {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+    return Math.floor(Math.random() * (max - min)) + min;
   };
 
   /* Элементы всплывающего окна настроек персонажа */
@@ -33,55 +16,44 @@
   var userNameFieldMaxLength = 50;
 
 
-  /* Мантия персонажа [ЭЛЕМЕНТ] */
-  var wizardCoatElement = document.querySelector('#wizard-coat');
-  /* Глаза персонажа [ЭЛЕМЕНТЫ] */
-  var wizardEyesElement = document.querySelector('#wizard-eyes');
-  /* Фаербол персонажа [ЭЛЕМЕНТ] */
-  var wizardFireballElement = document.querySelector('.setup-fireball-wrap');
+  /* Мантия персонажа [Данные] */
+  var wizardCoatData = {
+    element: document.querySelector('#wizard-coat'),
+    colors: [
+      'rgb(101, 137, 164)',
+      'rgb(241, 43, 107)',
+      'rgb(146, 100, 161)',
+      'rgb(56, 159, 117)',
+      'rgb(215, 210, 55)',
+      'rgb(0, 0, 0)'
+    ]
+  };
 
 
-  /* Все поддерживаемые цвета [Текстовый формат] */
-  var CSS_COLOR_NAMES = [
-    'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine',
-    'Azure', 'Beige', 'Bisque', 'Black',
-    'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown',
-    'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate',
-    'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson',
-    'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod',
-    'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki',
-    'DarkMagenta', 'DarkOliveGreen', 'Darkorange', 'DarkOrchid',
-    'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue',
-    'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet',
-    'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey',
-    'DodgerBlue', 'FireBrick', 'FloralWhite',
-    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite',
-    'Gold', 'GoldenRod', 'Gray', 'Grey',
-    'Green', 'GreenYellow', 'HoneyDew', 'HotPink',
-    'IndianRed', 'Indigo', 'Ivory', 'Khaki',
-    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon',
-    'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow',
-    'LightGray', 'LightGrey', 'LightGreen', 'LightPink',
-    'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
-    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue',
-    'LightYellow', 'Lime', 'LimeGreen', 'Linen',
-    'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue',
-    'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue',
-    'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue',
-    'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite',
-    'Navy', 'OldLace', 'Olive', 'OliveDrab',
-    'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod',
-    'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip',
-    'PeachPuff', 'Peru', 'Pink', 'Plum',
-    'PowderBlue', 'Purple', 'Red', 'RosyBrown',
-    'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown',
-    'SeaGreen', 'SeaShell', 'Sienna', 'Silver',
-    'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey',
-    'Snow', 'SpringGreen', 'SteelBlue', 'Tan',
-    'Teal', 'Thistle', 'Tomato', 'Turquoise',
-    'Violet', 'Wheat', 'White', 'WhiteSmoke',
-    'Yellow', 'YellowGreen'
-  ];
+  /* Глаза персонажа [Данные] */
+  var wizardEyesData = {
+    element: document.querySelector('#wizard-eyes'),
+    colors: [
+      'black',
+      'red',
+      'blue',
+      'yellow',
+      'green'
+    ]
+  };
+
+
+  /* Фаербол персонажа [Данные] */
+  var wizardFireballData = {
+    element: document.querySelector('.setup-fireball-wrap'),
+    colors: [
+      '#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'
+    ]
+  };
 
 
   /* Регистрация события на элементе всплывающего окна [КНОПКА ОТКРЫТЬ] */
@@ -112,23 +84,24 @@
 
 
   /* Регистрация события на элементе [МАНТИЯ ПЕРСОНАЖА] */
-  wizardCoatElement.addEventListener('click', function (event) {
+  wizardCoatData.element.addEventListener('click', function (event) {
     event.preventDefault();
 
-    wizardCoatElement.attributes.style.value = 'fill: rgb(' + getRandomInteger(1, 255) + ',' + getRandomInteger(1, 255) + ',' + getRandomInteger(1, 255) + ')';
+    wizardCoatData.element.attributes.style.value = 'fill: ' + wizardCoatData.colors[getRandomInteger(1, wizardCoatData.colors.length)];
   });
 
 
   /* Регистрация события на элементе [ГЛАЗА ПЕРСОНАЖА] */
-  wizardEyesElement.addEventListener('click', function (event) {
+  wizardEyesData.element.addEventListener('click', function (event) {
     event.preventDefault();
-    wizardEyesElement.setAttribute('fill', CSS_COLOR_NAMES[getRandomInteger(1, CSS_COLOR_NAMES.length)]);
+
+    wizardEyesData.element.setAttribute('fill', wizardEyesData.colors[getRandomInteger(1, wizardEyesData.colors.length)]);
   });
 
 
   /* Регистрация события на элементе [ОГНЕННЫЙ ШАР ПЕРСОНАЖА] */
-  wizardFireballElement.addEventListener('click', function (event) {
+  wizardFireballData.element.addEventListener('click', function (event) {
     event.preventDefault();
 
-    wizardFireballElement.setAttribute('style', 'background-color: #' + getRandomInteger(1, 255, true));
+    wizardFireballData.element.setAttribute('style', 'background-color:' + wizardFireballData.colors[getRandomInteger(1, wizardFireballData.colors.length)]);
   });
